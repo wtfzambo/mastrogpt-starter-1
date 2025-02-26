a4c43d26b283fff9b9e642557f391f5b23f4cb2f
wtfzambo <47462792+wtfzambo@users.noreply.github.com>
2025-02-26 10:51:59 +0000

    lesson 4 2nd half

--- packages/assistant/stateful/stateful.py
+++ packages/assistant/stateful/stateful.py
@@ -1,25 +1,27 @@
 import chat
+from history import History
 
 def stateful(args):
-  
+
   inp = args.get("input", "")
   out = f"Hello from {chat.MODEL}"
   res = {}
-  
+
   if inp != "":
     # load the history in the chat
     ch = chat.Chat(args)
-    #TODO:E4.2 load the history
-    #END TODO
-    # add a message and save it 
+    hi = History(args)
+    hi.load(ch)
+
+    # add a message and save it
     msg = f"user:{inp}"
     ch.add(msg)
+    hi.save(msg)
     print(ch.messages)
+
     out = ch.complete()
-    # complete, save the assistant and return the id
-    #TODO:E4.2 save the message and the state
-    # return the id as state field in the response
-    #END TODO
+    hi.save(f"assistant:{out}")
+    res['state'] = hi.id()
 
   res['output'] = out
   return res
